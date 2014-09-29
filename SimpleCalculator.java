bb7ce41429f8b87482607d2ada8ea53d0c1e1dac
jsalig1 <jeremy.salig@gmail.com>
2014-09-29 13:46:03 -0400

    Gets input, adds them together and prints result

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,5 +1,28 @@
+import java.io.*;
+
 public class SimpleCalculator {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String s1 = getInput("Enter a numeric value: ");
+    String s2 = getInput("Enter a numeric value: ");
+
+    double d1 = Double.parseDouble(s1);
+    double d2 = Double.parseDouble(s2);
+    double result = d1 + d2;
+
+    System.out.println("The answer is: " + result);
+  }
+
+  private static String getInput(String prompt) {
+    BufferedReader stdin = new BufferedReader(
+      new InputStreamReader(System.in));
+
+    System.out.print(prompt);
+    System.out.flush();
+
+    try {
+      return stdin.readLine();
+    } catch (Exception e) {
+      return "Error: " + e.getMessage();
+    }
   }
 }
