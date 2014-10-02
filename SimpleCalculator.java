8e56798d5420e6d0274cb21465a04e5aab3f1849
jsalig1 <jeremy.salig@gmail.com>
2014-10-02 18:47:59 -0400

    Handles number format errors, exits program, eliminated duplication

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -2,68 +2,73 @@ import java.io.*;
 
 public class SimpleCalculator {
   public static void main(String[] args) {
-    String s1 = getInput("Enter a numeric value: ");
-    String s2 = getInput("Enter a numeric value: ");
-    String operation = getInput("Select an operation. Valid entries: ADD, SUBTRACT, MULTIPLY, DIVIDE ");
+    String userInput1 = getInput("Enter a numeric value: ");
+    double num1 = setNum(userInput1);
+    String userInput2 = getInput("Enter a numeric value: ");
+    double num2 = setNum(userInput2);
+    String operation = getInput("Select an operation. Valid entries: ADD, SUBTRACT, MULTIPLY, DIVIDE: ");
     Operation value = setOpertion(operation);
 
     double result = 0;
 
     switch (value) {
       case ADD:
-        result = addValues(s1, s2);
+        result = addValues(num1, num2);
         break;
       case SUBTRACT:
-        result = subtractValues(s1, s2);
+        result = subtractValues(num1, num2);
         break;
       case MULTIPLY:
-        result = multiplyValues(s1, s2);
+        result = multiplyValues(num1, num2);
         break;
       case DIVIDE:
-        result = divideValues(s1, s2);
+        result = divideValues(num1, num2);
         break;
       case INVALID:
-        System.out.println("That was an invalid operation");
         return;
     }
 
     System.out.println("The answer is: " + result);
   }
-  
-  private static Operation setOpertion (String operation) throws NumberFormatException {
+
+  private static Operation setOpertion (String operation) throws IllegalArgumentException {
     try {
       Operation value = Operation.valueOf(operation);
       return value;
-    } catch (IllegalArgumentException e) {
+    } catch (IllegalArgumentException error) {
+      System.out.println("That was an invalid operation");
       return Operation.INVALID;
     }
   }
 
-  private static double addValues (String s1, String s2) throws NumberFormatException {
-    double d1 = Double.parseDouble(s1);
-    double d2 = Double.parseDouble(s2);
-    double result = d1 + d2;
+  private static double setNum (String userInput) throws NumberFormatException {
+    try {
+      double num = Double.parseDouble(userInput);
+      return num;
+    } catch (NumberFormatException error) {
+      System.out.println("That was not a valid number");
+      System.exit(0);
+      return 0;
+    }
+  }
+
+  private static double addValues (double num1, double num2) {
+    double result = num1 + num2;
     return result;
   }
 
-  private static double subtractValues (String s1, String s2) throws NumberFormatException {
-    double d1 = Double.parseDouble(s1);
-    double d2 = Double.parseDouble(s2);
-    double result = d1 - d2;
+  private static double subtractValues (double num1, double num2) {
+    double result = num1 - num2;
     return result;
   }
 
-  private static double multiplyValues (String s1, String s2) throws NumberFormatException {
-    double d1 = Double.parseDouble(s1);
-    double d2 = Double.parseDouble(s2);
-    double result = d1 * d2;
+  private static double multiplyValues (double num1, double num2) {
+    double result = num1 * num2;
     return result;
   }
 
-  private static double divideValues (String s1, String s2) throws NumberFormatException {
-    double d1 = Double.parseDouble(s1);
-    double d2 = Double.parseDouble(s2);
-    double result = d1 / d2;
+  private static double divideValues (double num1, double num2) {
+    double result = num1 / num2;
     return result;
   }
 
@@ -76,8 +81,8 @@ public class SimpleCalculator {
 
     try {
       return stdin.readLine();
-    } catch (Exception e) {
-      return "Error: " + e.getMessage();
+    } catch (Exception error) {
+      return "Error: " + error.getMessage();
     }
   }
 }
